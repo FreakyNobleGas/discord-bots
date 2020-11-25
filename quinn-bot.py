2c960df02c6b314677f1672e5b388e6cb4eb1e90
Nick Quinn <nickquinn3131@gmail.com>
2020-11-25 15:26:26 -0500

    Bot will only alert if voice channel has been empty longer than 30 seconds.

--- quinn-bot.py
+++ quinn-bot.py
@@ -1,17 +1,22 @@
 # Name: Nicholas Quinn
 # Created: 11/25/2020
-# Description: Discord bot that notifies the channel when a user joins a voice channel.
+# Description: Discord bot that notifies the channel when a user joins an empty voice channel.
 # Helpful Links: https://realpython.com/how-to-make-a-discord-bot-python/
 
 import os
-
 import discord
+import time
 from dotenv import load_dotenv
 
+# Grab secret env variables
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
+
+# Keep track of how often a user joins an empty voice channel
+global time_of_last_msg
+time_of_last_msg = time.time()
 
 @client.event
 async def on_ready():
@@ -22,8 +27,22 @@ async def on_ready():
 
 @client.event
 async def on_voice_state_update(member, before, after):
-    #print(f'{member.voice}')
-    if (member.voice is not None):
-        print(f'{member} just joined the {member.voice.channel} voice channel!')
+    # Define important variables
+    global time_of_last_msg
+    guild = discord.utils.get(client.guilds, name="FreakyNobleGas's server")
+    now = time.time()
+
+    # Go through each voice channel in the guild and determine if the voice channel was previously empty,
+    # it hasn't been empty longer than 30 seconds, and the member joined the channel.
+    for vc in guild.voice_channels:
+        if (member.voice is not None) and (len(vc.members) == 1) and ((now - time_of_last_msg) > 30.0):
+            for ch in guild.channels[0].text_channels:
+                if (ch.name == "general"):
+                    channel = ch
+            await channel.send(f'{member} just joined the {member.voice.channel} voice channel!')
+            print(f'{member.voice}')
+
+            # Update the last time a member joined an empty voice channel
+            time_of_last_msg = time.time()
 
 client.run(TOKEN)
