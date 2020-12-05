fb0fc29f5048d4d6a83eff184ad1db3013450bf9
Nick Quinn <nickquinn3131@gmail.com>
2020-12-05 00:01:46 -0500

    Commands now work!

--- ivo-bot/ivo-main.py
+++ ivo-bot/ivo-main.py
@@ -6,12 +6,32 @@ import os
 import discord
 import time
 from dotenv import load_dotenv
+from discord.ext import commands
 
 # Grab secret env variables
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
+
+#sets command prefix
+client = commands.Bot(command_prefix = '$')
+
+#loads specific cog
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+
+#unload specific cog
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+
+
+#iterates through cog files and loads them
+for filename in os.listdir('./ivo-bot/cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
 
 # Keep track of how often a user joins an empty voice channel
 global time_of_last_msg
@@ -28,13 +48,14 @@ async def on_ready():
 async def on_voice_state_update(member, before, after):
     # Define important variables
     global time_of_last_msg
+    
     guild = discord.utils.get(client.guilds, name="FreakyNobleGas's server")
     now = time.time()
 
     # Go through each voice channel in the guild and determine if the voice channel was previously empty,
     # it hasn't been empty longer than 30 seconds, and the member joined the channel.
     for vc in guild.voice_channels:
-        if (member.voice is not None) and (len(vc.members) == 1) and ((now - time_of_last_msg) > 30.0):
+        if (member.voice is not None) and (len(vc.members) == 1):
             for ch in guild.channels[0].text_channels:
                 if (ch.name == "general"):
                     channel = ch
