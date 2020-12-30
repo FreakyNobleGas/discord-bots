1d6415497166768341502422191f71e9ae641f84
Nick Quinn <nickquinn3131@gmail.com>
2020-12-29 21:39:57 -0500

    Added functionality to track user metrics for songs requested with groovy by storing locally in json file.

--- ivo-bot/ivo-main.py
+++ ivo-bot/ivo-main.py
@@ -5,6 +5,8 @@
 import os
 import discord
 import time
+import json
+import pathlib
 from dotenv import load_dotenv
 from discord.ext import commands
 
@@ -17,6 +19,36 @@ client = discord.Client()
 #sets command prefix
 client = commands.Bot(command_prefix = '$')
 
+try:
+    file = pathlib.Path("metrics.json")
+    if file.is_file():
+        print("Loading metrics file.")
+        metricsFile = open("metrics.json", "r+")
+    else:
+        print("No metrics file found. Creating new metrics file.")
+        metricsFile = open("metrics.json", "w+")
+        metricsFile.write("{}")
+        metricsFile.close()
+
+except Exception as e:
+    print("Error: Could not open file! Exiting...")
+    print(e)
+    exit()
+
+try:
+    # Get JSON object that holds metrics
+    print("Loading metrics JSON file.")
+    global metrics
+    with open("metrics.json") as metricsFile:
+        metrics = json.load(metricsFile,)
+    
+    metricsFile.close()    
+
+except Exception as e:
+    print("Error: Could not load metrics file. Exiting...")
+    print(e)
+    exit()
+
 #loads specific cog
 @client.command()
 async def load(ctx, extension):
@@ -26,7 +58,6 @@ async def load(ctx, extension):
 @client.command()
 async def unload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
-
 
 #iterates through cog files and loads them
 for filename in os.listdir('./ivo-bot/cogs'):
@@ -64,5 +95,51 @@ async def on_voice_state_update(member, before, after):
 
             # Update the last time a member joined an empty voice channel
             time_of_last_msg = time.time()
+
+@client.event
+async def on_message(message):
+    global metrics
+    print(type(metrics))
+
+    if "-play" in message.content:
+        author = str(message.author)
+        guild = str(message.channel.guild)
+        songPlayed = message.content[5:].strip()
+
+        print(author, " has requested the song ", songPlayed)
+
+        # Initialize guild if it is empty and add first user
+        if guild not in metrics.keys():
+            metrics[guild] = {
+                "users" : {
+                    author : {
+                        "totalSongs" : 1,
+                        "songsPlayed" : {
+                            songPlayed : 1
+                        }
+                    }
+                }
+            }
+        # Initialize user if this is their first song
+        elif author not in metrics[guild]["users"].keys():
+            metrics[guild]["users"][author] = {
+                "totalSongs" : 1,
+                "songsPlayed" : {
+                     songPlayed : 1
+                }
+            }
+        # Update previous user's total
+        else:
+            userData = metrics[str(guild)]["users"][str(author)]
+            userData["totalSongs"] = userData["totalSongs"] + 1
+
+            if songPlayed not in userData["songsPlayed"].keys():
+                userData["songsPlayed"][songPlayed] = 1
+            else:
+                userData["songsPlayed"][songPlayed] += 1
+        
+        with open("metrics.json", "w+") as metricsFile:
+            metricsFile.write(json.dumps(metrics))
+        metricsFile.close()
 
 client.run(TOKEN)
